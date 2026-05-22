608902841d307cdef79dd2aa46a458265cc68bb2
George Lin <linhairan@gmail.com>
2026-05-23 01:20:03 +0800

    Finish Assignment 3

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ gem 'sinatra', '~> 2.0'
 gem 'sinatra-contrib', '~> 2.0'
 gem 'erubi'
 gem 'webrick'
+gem 'pry'
 
 ruby '3.2.2'
--- cms.rb
+++ cms.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require 'sinatra/contrib'
+require 'sinatra/reloader'
 require 'tilt/erubi'
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do
-  "Getting started."
+  # @files = Dir.children("data/") # Original solution from myself
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+  erb :home, layout: :layout
 end
