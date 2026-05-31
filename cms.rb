defc7c5d7d831cf050f1f96ae914aa8bf23ada02
George Lin <linhairan@gmail.com>
2026-05-31 19:34:48 +0800

    My own attempt for Assignment 8

--- cms.rb
+++ cms.rb
@@ -18,12 +18,12 @@ def render_markdown(markdown_text)
   markdown_renderer.render(markdown_text)
 end
 
-def load_file_content(path)
-  content = File.read(path)
-  case File.extname(path)
+def load_file_content(file_path)
+  content = File.read(file_path)
+  case File.extname(file_path)
   when '.txt'
     headers['Content-Type'] = 'text/plain'
-    File.read(path)
+    File.read(file_path)
   when '.md'
     render_markdown(content)
   end
@@ -46,4 +46,19 @@ get '/:file_name' do
     session[:message] = "#{file_name} does not exist."
     redirect '/'
   end
+end
+
+get '/:file_name/edit' do
+  @file_name = params[:file_name]
+  file_path = root + '/data/' + @file_name
+  @file_content = File.read(file_path)
+  erb :edit, layout: :layout
+end
+
+post '/:file_name/edit' do
+  @file_name = params[:file_name]
+  file_path = root + '/data/' + @file_name
+  File.write(file_path, params['file_edit'])
+  session[:message] = "#{@file_name} has been updated."
+  redirect '/'
 end
